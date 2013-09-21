af07effe7ed6ae7b76a11b99c1f1cb54e32ff6df
Yuanhao_Yang <cs61c-al@hive18.CS.Berkeley.EDU>
2013-09-21 01:50:03 -0700

    lol

--- DoublePair.java
+++ DoublePair.java
@@ -1,6 +1,4 @@
-/*
- * CS 61C Fall 2013 Project 1
- *
+ /*
  * DoublePair.java is a class which stores two doubles and 
  * implements the Writable interface. It can be used as a 
  * custom value for Hadoop. To use this as a key, you can
@@ -15,12 +13,17 @@ import java.io.IOException;
 import org.apache.hadoop.io.Writable;
 
 public class DoublePair implements Writable {
+
     // Declare any variables here
+    private double d1;
+    private double d2;
 
     /**
      * Constructs a DoublePair with both doubles set to zero.
      */
     public DoublePair() {
+        this.d1 = 0;
+        this.d2 = 0;
         // YOUR CODE HERE
 
     }
@@ -29,6 +32,8 @@ public class DoublePair implements Writable {
      * Constructs a DoublePair containing double1 and double2.
      */ 
     public DoublePair(double double1, double double2) {
+        this.d1 = double1;
+        this.d2 = double2;
         // YOUR CODE HERE
 
     }
@@ -37,7 +42,8 @@ public class DoublePair implements Writable {
      * Returns the value of the first double.
      */
     public double getDouble1() {
-        // YOUR CODE HERE
+        return d1;
+    	// YOUR CODE HERE
         
     }
 
@@ -45,7 +51,8 @@ public class DoublePair implements Writable {
      * Returns the value of the second double.
      */
     public double getDouble2() {
-        // YOUR CODE HERE
+       return d2;
+    	// YOUR CODE HERE
         
     }
 
@@ -53,7 +60,8 @@ public class DoublePair implements Writable {
      * Sets the first double to val.
      */
     public void setDouble1(double val) {
-        // YOUR CODE HERE
+        this.d1 = val;
+    	// YOUR CODE HERE
 
     }
 
@@ -61,7 +69,8 @@ public class DoublePair implements Writable {
      * Sets the second double to val.
      */
     public void setDouble2(double val) {
-        // YOUR CODE HERE
+        this.d2 = val;
+    	// YOUR CODE HERE
 
     }
 
@@ -69,7 +78,9 @@ public class DoublePair implements Writable {
      * write() is required for implementing Writable.
      */
     public void write(DataOutput out) throws IOException {
-        // YOUR CODE HERE
+        out.writeDouble(this.d1);
+        out.writeDouble(this.d2);
+    	// YOUR CODE HERE
 
     }
 
@@ -77,7 +88,33 @@ public class DoublePair implements Writable {
      * readFields() is required for implementing Writable.
      */
     public void readFields(DataInput in) throws IOException {
-        // YOUR CODE HERE
+        this.d1 = in.readDouble();
+        this.d2 = in.readDouble();
+    	// YOUR CODE HERE
 
     }
+    
+    public int compareTo(Object doub) {
+    	DoublePair pair = (DoublePair) doub;
+        if (pair.d1 != this.d1) {
+        	return (pair.d1 < this.d1 ? 1 : -1);
+        } else {
+        	return Double.compare(this.d2, pair.d2);
+        }
+    }
+
+    public int hashCode() {
+        return Double.valueOf(d1 + d2).hashCode();
+    }
+    
+    public static void main(String[] args) {
+    	DoublePair dou1 = new DoublePair();
+    	System.out.println(dou1.d1);
+    	System.out.println(dou1.d2);
+    	dou1.setDouble1(1);
+    	dou1.setDouble2(2);
+    	System.out.println(dou1.getDouble2());
+    }
 }
+
+
